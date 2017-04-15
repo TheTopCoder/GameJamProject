7da21a39fa610d4fc62a662b62dbf95b2825db6f
Ronald Albert <ronaldalbert1609@gmail.com>
2017-04-15 02:51:31 -0300

    AddedMenuScenre

--- GameJamProject/Assets/Scripts/MenuScript.cs
+++ GameJamProject/Assets/Scripts/MenuScript.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuScript : MonoBehaviour
 {
     [SerializeField]
     Canvas exitCanvas;
+    [SerializeField]
+    GameObject EventSytem;
+    [SerializeField]
+    GameObject yesButton;
+    [SerializeField]
+    GameObject playButton;
+    [SerializeField]
+    GameObject creditsButton;
+    [SerializeField]
+    GameObject exitButton;
+
     Canvas thisCanvas;
     void Awake()
     {
@@ -27,6 +39,10 @@ public class MenuScript : MonoBehaviour
     {
         exitCanvas.enabled = true;
         this.GetComponent<Canvas>().enabled = false;
+        playButton.GetComponent<Button>().enabled = false;
+        creditsButton.GetComponent<Button>().enabled = false;
+        exitButton.GetComponent<Button>().enabled = false;
+        EventSytem.GetComponent<EventSystem>().SetSelectedGameObject(yesButton);
     }
 
     public void OnYes()
@@ -38,6 +54,10 @@ public class MenuScript : MonoBehaviour
     {
         exitCanvas.enabled = false;
         this.GetComponent<Canvas>().enabled = true;
+        playButton.GetComponent<Button>().enabled = true;
+        creditsButton.GetComponent<Button>().enabled = true;
+        exitButton.GetComponent<Button>().enabled = true;
+        EventSytem.GetComponent<EventSystem>().SetSelectedGameObject(playButton);
     }
 
     
