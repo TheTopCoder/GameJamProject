5f387e1efe261b5a8d4a14b7bcd597dae23fc827
Ronald Albert <ronaldalbert1609@gmail.com>
2017-04-15 02:29:45 -0300

    AddedMenuAndCreditsScene

---
+++ GameJamProject/Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CreditsScript : MonoBehaviour
+{
+    public void OnBackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
---
+++ GameJamProject/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField]
+    Canvas exitCanvas;
+    Canvas thisCanvas;
+    void Awake()
+    {
+        exitCanvas.enabled = false;
+    }
+    public void OnPlay()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+
+    public void OnCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void OnExit()
+    {
+        exitCanvas.enabled = true;
+        this.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void OnYes()
+    {
+        Application.Quit();
+    }
+
+    public void OnNo()
+    {
+        exitCanvas.enabled = false;
+        this.GetComponent<Canvas>().enabled = true;
+    }
+
+    
+}
