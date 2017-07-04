a8c46d3a7689a3c2d4f91aa8c5426fd5cf6fd6dc
TheTopCoder <lucas.brito.reis@hotmail.com>
2017-07-04 17:42:44 -0300

    MaloomGT_Build

---
+++ GameJamProject/Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectWithTag ("PlayerBase");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		GameObject.FindGameObjectWithTag ("PlayerBase");
+	}
+}
